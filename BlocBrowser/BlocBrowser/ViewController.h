702f432d81e4fddabb95cf51d0b4d11ee4c478d5
Eddy Chan <echan162@gmail.com>
2016-04-20 14:36:48 -0700

    Checkpoint 23 bloc browser web view: completed

---
+++ BlocBrowser/BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Eddy Chan on 4/19/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
